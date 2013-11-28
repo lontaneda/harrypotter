e12b11931f4889daf79e7b9794c4b3a4314437de
lontaneda <lontaneda@infoblock.mx>
2013-11-28 09:18:10 -0600

    Refactorization for data context

--- HarryPotter/HarryPotter.Data.Tests/DataContextTestsAuthors.cs
+++ HarryPotter/HarryPotter.Data.Tests/DataContextTestsAuthors.cs
@@ -1,37 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
-using ImproveIT.Data;
 using System.Data;
 using HarryPotter.Domain;
-using System.Configuration;
-using NDbUnit.Core;
 
 namespace HarryPotter.Data.Tests
 {
 
     [TestFixture]
     public class DataContextTestsAuthors
+		: DataContextTests
     {
-
-        private IDataContext _dataContext;
-        private INDbUnitTest _ndbUnitTest;
-
-        [SetUp]
-        public void Setup()
-        {
-            string cnn = ConfigurationManager.ConnectionStrings["storedb_development"].ConnectionString;
-            this._ndbUnitTest = new NDbUnit.Core.SqlClient.SqlDbUnitTest(cnn);
-            this._ndbUnitTest.ReadXmlSchema("StoreSchema.xsd");
-            this._ndbUnitTest.PerformDbOperation(NDbUnit.Core.DbOperationFlag.DeleteAll);
-
-            NHibernate.ISession session =
-                HarryPotter.Data.DataContextBuilder.BuildSession();
-            this._dataContext = new ImproveIT.Data.Hibernate.HibernateDataContext(session);
-        }
-
         [Test]
         public void Add_EmptyDatabase_OneRowInserted()
         {
--- HarryPotter/HarryPotter.Data.Tests/DataContextTestsBooks.cs
+++ HarryPotter/HarryPotter.Data.Tests/DataContextTestsBooks.cs
@@ -1,35 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ImproveIT.Data;
-using NDbUnit.Core;
-using NUnit.Framework;
-using System.Configuration;
+﻿using NUnit.Framework;
 using System.Data;
 using HarryPotter.Domain;
 
 namespace HarryPotter.Data.Tests
 {
     public class DataContextTestsBooks
+		: DataContextTests
     {
-
-        private IDataContext _dataContext;
-        private INDbUnitTest _ndbUnitTest;
-
-        [SetUp]
-        public void Setup()
-        {
-            string cnn = ConfigurationManager.ConnectionStrings["storedb_development"].ConnectionString;
-            this._ndbUnitTest = new NDbUnit.Core.SqlClient.SqlDbUnitTest(cnn);
-            this._ndbUnitTest.ReadXmlSchema("StoreSchema.xsd");
-            this._ndbUnitTest.PerformDbOperation(NDbUnit.Core.DbOperationFlag.DeleteAll);
-
-            NHibernate.ISession session =
-                HarryPotter.Data.DataContextBuilder.BuildSession();
-            this._dataContext = new ImproveIT.Data.Hibernate.HibernateDataContext(session);
-        }
-
         [Test]
         public void Add_NoBooksInDatabase3Author_OneBookInsertedWithAuthor()
         {
